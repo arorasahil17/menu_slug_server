92dd784d8e4f7d0b527412ee7ed326daee17069c
= <=>
2023-12-06 21:10:44 +0530

    static changes

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const app = express();
 const port = 3000;
 const menuRouter = require("./routes");
 const cors = require("cors");
+const path = require("path");
 
 connectDb();
 
+app.use(express.static(path.resolve(__dirname, "dist")));
 app.use(cors());
 app.use(express.json());
 app.use("/api", menuRouter);
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "dist", "index.html"))
+);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
